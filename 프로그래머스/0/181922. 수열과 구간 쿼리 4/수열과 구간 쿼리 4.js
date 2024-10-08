d8d974d4f6a08ede511e73f2d9efe9082c05f3d3
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-10-08 23:52:15 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 7.59 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,12 +1,7 @@
 function solution(arr, queries) {
-  var answer = [];
-  for (var i = 0; i < queries.length; i++) {
-    var s = queries[i][0];
-    var e = queries[i][1];
-    var k = queries[i][2];
-
-    for (let j = s; j <= e; j++) {
-      if (j % k == 0) arr[j] += 1;
+  for (let [s, e, k] of queries) {
+    for (let i = s; i <= e; i++) {
+      if (i % k == 0) arr[i]++;
     }
   }
 
