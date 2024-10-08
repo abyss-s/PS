0a15371162122307b60a7142561439e44020659b
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-10-09 00:06:28 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  var answer = [];
+  answer.push(n);
+  while (n != 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n = n * 3 + 1;
+    }
+    answer.push(n);
+  }
+  return answer;
+}
