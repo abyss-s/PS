d85e5ad6c5abf5f6d618227e64e94580f7346185
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-10-12 03:20:35 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t;
+  cin >> t;
+  int dp0[41] = {0};
+  int dp1[41] = {0};
+
+  dp0[0] = 1;
+  dp1[0] = 0;
+  dp0[1] = 0;
+  dp1[1] = 1;
+
+  for (int i = 2; i <= 40; i++) {
+    dp0[i] = dp0[i - 1] + dp0[i - 2];
+    dp1[i] = dp1[i - 1] + dp1[i - 2];
+  }
+
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << dp0[n] << " " << dp1[n] << '\n';
+  }
+
+  return 0;
+}
