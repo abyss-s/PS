fcdd700c9949007d24778283e09c981ab877add5
Youngju Lee <77565980+abyss-s@users.noreply.github.com>
2025-01-18 22:34:41 +0900

    [level 0] Title: 접미사 배열, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -2,7 +2,7 @@ function solution(my_string) {
     var answer = [];
     len = my_string.length;
     for(let i =0; i < len; i++)
-        answer.push(my_string.slice(len-i-1));
+        answer.push(my_string.substr(i));
     answer.sort();
     return answer;
 }
