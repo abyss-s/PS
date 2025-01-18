a25a2060db9be12fbc350d301f9595eac85adce4
Youngju Lee <77565980+abyss-s@users.noreply.github.com>
2025-01-18 22:40:15 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,11 +1,3 @@
 function solution(my_string, is_suffix) {
-    arr = [];
-    len = my_string.length;
-    for(i = 0; i < len; i++)
-        arr.push(my_string.substr(i));
-    if(arr.includes(is_suffix))
-        return 1;
-    else
-        return 0;
-
+    return my_string.endsWith(is_suffix) ? 1 : 0;
 }
