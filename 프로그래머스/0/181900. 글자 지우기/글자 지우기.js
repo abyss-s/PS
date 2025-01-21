eec541645c2afc01e292dc4977e3427655d78555
Youngju Lee <77565980+abyss-s@users.noreply.github.com>
2025-01-21 14:36:46 +0900

    [level 0] Title: 글자 지우기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,3 +1,3 @@
 function solution(my_string, indices) {
-    return my_string.split('').filter((_, i) => (!indices.includes(i))).join('');
+    return [...my_string].filter((_, i) => (!indices.includes(i))).join('');
 }
