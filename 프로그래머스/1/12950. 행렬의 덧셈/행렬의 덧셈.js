ef52d30950f75543e28652db9367b3279af6a798
Youngju Lee <lh3729@naver.com>
2025-01-26 13:26:35 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.09 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,4 @@
 function solution(arr1, arr2) {
-    var sum = [];
-    for (let i = 0; i < arr1.length; i++) {
-        var row = [];
-        for (let j = 0; j < arr1[i].length; j++) {
-            row.push(arr1[i][j] + arr2[i][j]);
-        }
-        sum.push(row);
-    }
-    return sum;
+    return arr1.map((row, i) =>
+        row.map((val, j) => val + arr2[i][j]));
 }
