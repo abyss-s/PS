1d1e1176babb921e28bce4d76bb2d75056c740b8
Youngju Lee <lh3729@naver.com>
2025-02-21 12:36:53 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// 백준 제출용
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// 로컬 테스트용
+// const filePath =
+//   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// 백준 11053번: 가장 긴 증가하는 부분 수열
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+let dp = new Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+  }
+}
+
+console.log(Math.max(...dp));
