10a7b55245e67b68a5b44a1cd91f48f9f24bc0af
Youngju Lee <lh3729@naver.com>
2025-03-03 23:18:32 +0900

    [Gold V] Title: 강의실 배정, Time: 632 ms, Memory: 68092 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,16 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		int[][] v = new int[n][2]; // si, ti
+
 		for (int i = 0; i < n; i++) {
-			v[i][0] = sc.nextInt();
-			v[i][1] = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			v[i][0] = Integer.parseInt(st.nextToken());
+			v[i][1] = Integer.parseInt(st.nextToken());
 		}
 
 		Arrays.sort(v, Comparator.comparingInt(a -> a[0]));
@@ -18,10 +22,10 @@ public class Main {
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		pq.add(v[0][1]);
 		for (int i = 1; i < n; i++) {
-			if (pq.peek() <= v[i][0]) { // top(ti) <= si
-				pq.poll(); // pop(유지)
+			if (pq.peek() <= v[i][0]) {
+				pq.poll();
 			}
-			pq.add(v[i][1]); // add
+			pq.add(v[i][1]);
 		}
 		System.out.println(pq.size());
 	}
