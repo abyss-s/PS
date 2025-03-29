7c88ba935472ffde4f8d5604d3dd461b6eae6dc4
Youngju Lee <lh3729@naver.com>
2025-03-29 10:41:36 -0700

    [Gold V] Title: 수 고르기, Time: 2604 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -2,15 +2,16 @@ n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 arr.sort()
 left = 0
-right = 0
+right = 1
 minDiff = float('inf')
 while left < n and right < n:
     diff = arr[right]-arr[left]
+    if diff == m:
+        print(m)
+        exit(0)
     if diff >= m:
         minDiff = min(diff, minDiff)
         left+=1
     else:
-        right+=1
-    if left == right:
         right+=1
 print(minDiff)
