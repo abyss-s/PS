5cc03f18e5d37473296d9d89755533c720b143ed
Youngju Lee <lh3729@naver.com>
2025-08-14 16:39:37 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+# 2823번: 코드마스터 2023
+a = input()
+if a == "SONGDO":
+    print("HIGHSCHOOL")
+elif a == "CODE":
+    print("MASTER")
+elif a == "2023":
+    print("0611")
+elif a == "ALGORITHM":
+    print("CONTEST")
