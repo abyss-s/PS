77e910728add78f5b43d7c392ab0e2e17a8f9ec7
Youngju Lee <lh3729@naver.com>
2025-09-08 23:47:31 +0900

    [Silver I] Title: 쿼드트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = [list(input().strip()) for _ in range(n)]
+
+
+def recur(x, y, size):
+    s = arr[x][y]
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if arr[i][j] != s:
+                mid = size // 2
+                return ("(" + recur(x, y, mid) + recur(x, y + mid, mid)
+                        + recur(x + mid, y, mid) + recur(x + mid,
+                                                         y + mid,
+                                                         mid) + ")")
+    return s
+
+
+print(recur(0, 0, n))
