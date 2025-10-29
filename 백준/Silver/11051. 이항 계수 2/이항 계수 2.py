5206401caa9e6f7ef6824195ed4758f878b5e6b4
Youngju Lee <lh3729@naver.com>
2025-10-29 23:45:58 +0900

    [Silver II] Title: 이항 계수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+res = 1
+for i in range(k):
+    res *= n
+    n -= 1
+
+div = 1
+for i in range(2, k + 1):
+    div *= i
+
+print((res // div) % 10007)
